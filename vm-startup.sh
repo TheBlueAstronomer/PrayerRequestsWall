572df83f5ede1d9066e20ac69184bcbed8318f14
Jeffrey Ronald <TheBlueAstronomer@users.noreply.github.com>
2026-03-06 00:00:39 +0530

    chore: cleanup deploy script

--- vm-startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
-curl -SL https://github.com/docker/compose/releases/download/v2.24.6/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-systemctl enable docker
-systemctl start docker
